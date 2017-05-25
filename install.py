c89d2c48de6b7d542c1be8c6017ffa7ac2228c78
IlyasDeckers <ilyas@phasehosting.io>
2017-05-25 12:44:06 +0200

    install.sh

--- install.py
+++
@@ -1,9 +0,0 @@
-#!flask/bin/python
-
-from app.database.db_connect import DB
-
-conn = DB().connect()
-conn.execute('CREATE TABLE users (name TEXT, password TEXT)')
-conn.close()
-
-print "Install successful"
